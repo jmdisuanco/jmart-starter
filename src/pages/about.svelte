e7ea12326cb76d4461276d89d5c979db42141510
JM <jm@disuan.co>
2019-12-12 17:14:11 +0400

    remove unecesaries

--- src/pages/about.svelte
+++ src/pages/about.svelte
@@ -1,9 +1,4 @@
-<script>
-  import states from "../states";
-  let { page } = states;
-</script>
-
-<div id="indexPage">
+<div id="aboutPage">
   <h1 id="header" class="text-6xl">About JMart</h1>
   <a id="testlink" href="/index">Home</a>
 </div>
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,8 +1,3 @@
-<script>
-  import states from "../states";
-  let { page } = states;
-</script>
-
 <div id="indexPage">
   <h1 id="header" class="text-6xl">Welcome to JMart</h1>
   <a id="testlink" href="/about">About</a>
